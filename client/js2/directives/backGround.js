7f4f5d40acb4e6c03c7eef8671f4a7a7aabaee4b
James Langford <derivative7117@live.com>
2017-05-10 17:24:48 -0600

    12th commit

---
+++ client/js2/directives/backGround.js
@@ -0,0 +1,79 @@
+angular.module('ecom').directive('backGround', function ($timeout, $interval) {
+    return {
+        restrict: 'A',
+        link: (scope, elem, attr) => {
+            elem.css({'background-image': './images/image1.jpeg'});
+            let backgrounds = [
+                './js2/images/image2.jpg',
+                './js2/images/image3.jpg',
+                './js2/images/image4.jpg',
+                './js2/images/image5.JPG',
+                './js2/images/image6.jpg',
+                './js2/images/image7.jpg',
+                './js2/images/image8.jpg',
+                './js2/images/image9.jpg',
+                './js2/images/image10.jpg',
+                './js2/images/image1.jpeg'
+            ];
+            let i = -1;
+
+            $interval(() => {
+                i++;
+                if (i === backgrounds.length - 1) {
+                    i = 0;
+                }
+                let bg_string = "url(" + backgrounds[i] + ")";
+                elem.css({
+                    'background-image': bg_string,
+                    'background-size': 'cover',
+                    'background-position': 'center',
+                    'background-attachment': 'fixed',
+                    'background-repeat': 'no-repeat',
+                    'height': '100%',
+                    'width': '100%',
+                    'transition': '.7s',
+                    // '-webkit-transition': 'opacity 3s ease-in-out',
+                    '-webkit-backface-visibility': 'hidden',
+                    '-webkit-transform': 'translateZ(0) scale(1.0, 1.0)'
+                });
+            }, 5500)
+        }
+    }
+})
+
+//         // let base = 'https://images.pexels.com/photos/164938/pexels-photo-164938.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb',
+//             secs = 4;
+//         backgrounds.forEach(function(img){
+//             new Image().src = base + img;
+//             // caches images, avoiding white flash between background replacements
+//         });
+//
+//         function backgroundSequence() {
+//             window.clearTimeout();
+//             var k = 0;
+//             for (let i = 0; i < backgrounds.length; i++) {
+//                 setTimeout(function(){
+//                     console.log('working');
+//                     attr.style.background = "url(" + base + backgrounds[k] + ") no-repeat center center fixed";
+//                     attr.style.backgroundSize ="cover";
+//                     if ((k + 1) === backgrounds.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }
+//                 }, (secs * 1000) * i)
+//             }
+//         }
+//         backgroundSequence();
+//
+//
+//
+//
+//     //         current = 0;
+//     //     let nextBackground = () => {
+//     //         elem.css('background',
+//     //             backgrounds[current = ++current % backgrounds.length]
+//     //         );
+//     //         setTimeout(nextBackground, 1000);
+//     //     }
+//     //     setTimeout(nextBackground, 1000);
+//     //     elem.css('background', backgrounds[0]);
+//      }
+
+
--- client/js2/directives/fade.js
+++ client/js2/directives/fade.js
@@ -1,15 +1,22 @@
 angular.module('ecom').directive('fade', function () {
     return {
         restrict: 'A',
-        link: function(scope, elem, attr) {
-            elem.on('click', function () {
-                elem.mouseenter(() => {
-                    elem.fadeOut('slow');
+        link: function (scope, elem, attr) {
+            elem.mouseleave(() => {
+                elem.css({
+                    'transition': '.750s',
+                    'box-shadow': '0 0 0 0 rgba(0,0,0,0)',
+                    'opacity':'0'
+
+                });
+            })
+            elem.mouseenter(() => {
+                elem.css({
+                    'transition': '.750s',
+                    'box-shadow': '0 2px 5px 0 rgba(0,0,0,.16)',
+                    'opacity': '1'
                 })
             })
-            // elem.mouseenter( function() {
-            //     elem.hide(4000);
-            // })
         }
     }
 });
