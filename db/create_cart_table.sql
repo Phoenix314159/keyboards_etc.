df16c5f2dbde72cca1e01c5128db3d645453aa15
James Langford <derivative7117@live.com>
2017-05-11 01:24:30 -0600

    14th commit

--- db/create_cart_table.sql
+++ db/create_cart_table.sql
@@ -13,6 +13,41 @@ set totalprice = $1;
 
 select distinct totalprice from cart;
 
+update cart
+set totalprice = $1
+from(select * from products
+     join cart
+     on products.id = cart.productId
+);
+update cart set totalprice = (
+select price, productId from products
+join cart
+on products.id = cart.productId(select price where productid = $1)
+)
+update cart set totalprice = (
 select * from products
 join cart
-on products.id = cart.productId;
+on products.id = cart.productId where productid = 2
+)
+
+(
+update cart.totalprice
+set totalprice = $1;
+ )
+
+ update cart set totalprice = $1
+ from products
+ where cart.productId = products.id;
+
+UPDATE cart INNER JOIN products on cart.productId = products.id
+SET cart.totalprice = products.price
+WHERE table1.column3 = 'randomCondition';
+
+UPDATE cart
+  SET cart.totalprice = products.price
+  FROM cart AS a
+  INNER JOIN products AS b
+  ON a.productId = b.id;
+
+
+
--- db/update_total.sql
+++ db/update_total.sql
@@ -1,2 +1,5 @@
 update cart
-set totalprice = $1;
+set totalprice = $1
+from cart as a
+inner join products as b
+on a.productId = b.id;
