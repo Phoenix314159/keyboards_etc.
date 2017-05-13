c8d2d5e0d89a37bc14894e6199b478c6159c0a1d
James Langford <derivative7117@live.com>
2017-05-13 15:49:29 -0600

    22nd commit

--- client/js2/directives/backGround.js
+++ client/js2/directives/backGround.js
@@ -23,56 +23,15 @@ angular.module('ecom').directive('backGround', function ($timeout, $interval) {
                     i = 0;
                 }
                 let bg_string = "url(" + backgrounds[i] + ")";
+
                 elem.css({
                     'background-image': bg_string,
-                    'background-size': 'cover',
-                    'background-position': 'center',
-                    'background-attachment': 'fixed',
-                    'background-repeat': 'no-repeat',
-                    'height': '100%',
-                    'width': '100%',
-                    'transition': '1.5s',
-                    '-webkit-backface-visibility': 'hidden',
-                    '-webkit-transform': 'translateZ(0) scale(1.0, 1.0)',
-                    '-webkit-animation': 'fadein 1.5s' });
+
+                });
+
             }, 7500)
         }
     }
 })
-
-//         // let base = 'https://images.pexels.com/photos/164938/pexels-photo-164938.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb',
-//             secs = 4;
-//         backgrounds.forEach(function(img){
-//             new Image().src = base + img;
-//             // caches images, avoiding white flash between background replacements
-//         });
-//
-//         function backgroundSequence() {
-//             window.clearTimeout();
-//             var k = 0;
-//             for (let i = 0; i < backgrounds.length; i++) {
-//                 setTimeout(function(){
-//                     console.log('working');
-//                     attr.style.background = "url(" + base + backgrounds[k] + ") no-repeat center center fixed";
-//                     attr.style.backgroundSize ="cover";
-//                     if ((k + 1) === backgrounds.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }
-//                 }, (secs * 1000) * i)
-//             }
-//         }
-//         backgroundSequence();
-//
-//
-//
-//
-//     //         current = 0;
-//     //     let nextBackground = () => {
-//     //         elem.css('background',
-//     //             backgrounds[current = ++current % backgrounds.length]
-//     //         );
-//     //         setTimeout(nextBackground, 1000);
-//     //     }
-//     //     setTimeout(nextBackground, 1000);
-//     //     elem.css('background', backgrounds[0]);
-//      }
 
 
